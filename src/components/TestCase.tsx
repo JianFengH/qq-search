41d5f2534a52d90bc8e8731788955fe03b61f37c
JianFengH <707156268@qq.com>
2022-05-10 22:25:07 +0800

    Add TestCase

---
+++ src/components/TestCase.tsx
@@ -0,0 +1,18 @@
+type UserProps = {
+  name: string
+  age: number
+  sex: string
+}
+
+type keysOfUser = keyof UserProps;
+// type keysOfUser2 = 'name' | 'age' | 'sex';
+
+function TestUser() {
+  let key: keysOfUser = 'name';
+  console.log(key);
+  return <>
+    {key}
+  </>
+}
+
+export default TestUser;
